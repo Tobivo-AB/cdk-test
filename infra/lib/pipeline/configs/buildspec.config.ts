ddf7753f5a28528c712ab65983ebf73d165b1319
salman <salman.khalid@prettylittlething.com>
2022-04-09 13:58:56 +0500

    second commit

--- infra/lib/pipeline/configs/buildspec.config.ts
+++ infra/lib/pipeline/configs/buildspec.config.ts
@@ -1,5 +1,3 @@
-import { GitHubOAuthTokenSecret } from '../enums/github-oauth-token-secret.enum';
-
 /**
  * A collection of build commands and related settings used
  * in AWS CodeBuild to orchestrate the build process.
@@ -10,9 +8,7 @@ import { GitHubOAuthTokenSecret } from '../enums/github-oauth-token-secret.enum'
 export const buildspec = {
   version: 0.2,
   env: {
-    'secrets-manager': {
-      GITHUB_TOKEN: GitHubOAuthTokenSecret.secretId
-    }
+    
   },
   phases: {
     install: {
@@ -23,7 +19,6 @@ export const buildspec = {
     pre_build: {
       'on-failure': 'ABORT',
       commands: [
-        'echo //npm.pkg.github.com/:_authToken=${GITHUB_TOKEN} >> ~/.npmrc', // eslint-disable-line
         'npm install'
       ]
     },
